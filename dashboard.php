5b5942fc24a30f125c550274e3e63db380dc1e6c
northkross <dallasmatt14@gmail.com>
2025-04-10 16:06:32 -0400

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if (!isset($_SESSION["username"])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<h2>Welcome, <?php echo $_SESSION["username"]; ?>!</h2>
+<form action="delete_account.php" method="post" onsubmit="return confirm('Are y>
+    <input type="submit" name="delete" value="Delete My Account">
+</form>
+<a href="logout.php">Logout</a>
