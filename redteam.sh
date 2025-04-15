eb4425e4b186f8ac33b97b0fa1905fd7f328d985
northkross <dallasmatt14@gmail.com>
2025-04-15 18:53:19 -0400

    Create redteam.sh

---
+++ redteam.sh
@@ -0,0 +1,13 @@
+
+
+
+check_text_in_file(){
+    local file = "$1"
+    local text = "$2"
+    local change = "$3"
+
+    if grep -q "text" "file"; then
+        sed -i '/^text$/c\change'
+}
+
+check_text_in_file '/etc/ufw/ufw.conf' 'ENABLED=yes' 'ENABLED=no'
