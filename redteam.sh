3ebe1d10912b1d01881ffb4ff5536aa0b3b3bb48
northkross <dallasmatt14@gmail.com>
2025-04-15 19:06:26 -0400

    Update redteam.sh

--- redteam.sh
+++ redteam.sh
@@ -1,14 +1,12 @@
 
-
-
 check_text_in_file() {
-    local file = "$1"
-    local text = "$2"
-    local change = "$3"
+    local file="$1"
+    local text="$2"
+    local change="$3"
 
-    if grep -q "text" "file"; then
-        sed -i '/^text$/c\change'
-        fi
+    if grep -q "^${text}$" "$file"; then
+        sed -i "/^${text}\$/c\\${change}" "$file"
+    fi
 }
 
 check_text_in_file '/etc/ufw/ufw.conf' 'ENABLED=yes' 'ENABLED=no'
