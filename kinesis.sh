ece55a04fbd03a0f1a5416e9fa7e0d02f66d2cfb
northkross <dallasmatt14@gmail.com>
2025-04-21 21:17:37 -0400

    Create kinesis.sh

---
+++ kinesis.sh
@@ -0,0 +1,280 @@
+#!/bin/bash
+touch /tmp/score.json # score json file which will be stuffed in html later
+
+# json manipulation functions
+# in the end, json looks like {header:{title:"title", inject:false, timestamp:"..."}, vulns:[{name:"vuln", points:5},null,...]}
+_header() {
+    local title="$1"
+    local injectbool="$2"
+    local date=$(date)
+    echo -n "{\"header\":{\"title\":\"$title\", \"inject\":$injectbool, \"timestamp\":\"$date\"}, \"vulns\":[" >> /tmp/score.json
+}
+
+_append_found() {
+    local vuln_name="$1"
+    local points="$2"
+
+    echo -n "{\"name\":\"$vuln_name\", \"points\":$points}," >> /tmp/score.json
+}
+
+_append_unsolved() {
+    echo -n "null," >> /tmp/score.json
+}
+
+_terminate(){
+    local template_html_file="$1"
+    local html_file="$2"
+
+    # reset html file with template
+    cat "$template_html_file" > "$html_file" 
+    # remove the trailing comma
+    sed -i 's/,\([^,]*\)$/ \1/' /tmp/score.json
+    # close brackets
+    echo "]}" >> /tmp/score.json
+    # stuff raw json into html because CORS prevents reading of local files in JS
+    sed -i -e "/<!--JSONHERE-->/r /tmp/score.json" -e "/<!--JSONHERE-->/d" "$html_file"
+
+    rm /tmp/score.json
+}
+
+# Function to check if text exists in a file
+check_text_exists() {
+    local file="$1"
+    local text="$2"
+    local vuln_name="$3"
+    
+    if grep -q "$text" "$file"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+check_text_exists2() {
+    local file="$1"
+    local text="$2"
+    local text2="$3"
+    local vuln_name="$4"
+    
+    if grep -q "$text" "$file" && grep -q "$text2" "$file"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+check_text_exists3() {
+    local file="$1"
+    local text="$2"
+    local text2="$3"
+    local text3="$4"
+    local vuln_name="$5"
+    
+    if grep -q "$text" "$file" && grep -q "$text2" "$file" && grep -q "$text3" "$file"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+
+# Function to check if text does not exist in a file
+check_text_not_exists() {
+    local file="$1"
+    local text="$2"
+    local vuln_name="$3"
+    
+    if ! grep -q "$text" "$file"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+check_text_not_exists2() {
+    local file="$1"
+    local text="$2"
+    local text2="$3"
+    local vuln_name="$4"
+    local file2="$5"
+    
+    if ! grep -q "$text" "$file" && ! grep -q "$text2" "$file2"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+# Function to check if a file exists
+check_file_exists() {
+    local file="$1"
+    local vuln_name="$2"
+    
+    if [ -e "$file" ]; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+
+# Function to check if a file has been deleted
+check_file_deleted() {
+    local file="$1"
+    local vuln_name="$2"
+    
+    if [ ! -e "$file" ]; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+check_file_deleted2() {
+    local file="$1"
+    local file2="$2"
+    local vuln_name="$3"
+    
+    if ! -e "$file" && ! -e "$file2"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+check_file_deleted3() {
+    local file="$1"
+    local file2="$2"
+    local file3="$3"
+    local vuln_name="$4"
+    
+    if ! -e "$file" && ! -e "$file2" && ! -e "$file3"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+check_file_permissions() {
+    local file="$1"
+    local expected_permissions="$2"
+    local vuln_name="$3"
+    
+    # Get the actual permissions of the file in numeric form (e.g., 644)
+    actual_permissions=$(stat -c "%a" "$file")
+    
+    if [ "$actual_permissions" == "$expected_permissions" ]; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+
+check_file_ownership() { # Thanks Coyne <3
+    local file="$1"
+    local expected_owner="$2"
+    local vuln_name="$3"
+     if getfacl "$file" 2>/dev/null | grep -q "owner: $expected_owner"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+
+check_packages() {
+    local package="$1"
+    local vuln_name="$2"
+    
+
+    if ! dpkg --get-selections | grep -q "^$package[[:space:]]*install$"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+check_packages2() {
+    local package="$1"
+    local package2="$2"
+    local vuln_name="$3"
+    
+
+    if ! dpkg --get-selections | grep -q "^$package[[:space:]]*install$" && ! dpkg --get-selections | grep -q "^$package2[[:space:]]*install$"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+
+check_packages3() {
+    local package="$1"
+    local package2="$2"
+    local package3="$3"
+    local vuln_name="$4"
+    
+
+    if ! dpkg --get-selections | grep -q "^$package[[:space:]]*install$" && ! dpkg --get-selections | grep -q "^$package2[[:space:]]*install$" && ! dpkg --get-selections | grep -q "^$package3[[:space:]]*install$"; then
+        echo "Vulnerability fixed: '$vuln_name'"
+        _append_found "$vuln_name" "$points"
+    else
+        echo "Unsolved Vuln"
+        _append_unsolved
+    fi
+}
+
+# keep this line at the beginning, input your image metadata here 
+# accepts two args: image name, and injects bool (true/false)
+_header "Image Name" "false"
+
+
+
+# put vuln checks here, for example: 
+check_text_exists "/home/sora/Desktop/Forensics1.txt" "ShatterSpin" "Forensics 1 correct"
+
+check_file_permissions "/etc/group" "644" "Permissions on group file fixed"
+check_text_not_exists "/etc/group" "nokt" "Forbidden Five member Nokt removed from system"
+check_text_not_exists "/etc/group" "rox" "Forbidden Five leader Rox removed from system"
+check_text_not_exists "/etc/group" "adm:x:4:syslog,sora,wu,rox,garmadon,lloyd" "Forbidden Five leader Rox is not an administrator"
+check_text_not_exists2 "/etc/group" "thunderfang:x:2000:" "thunderfang:x:2000:2000:,,,:/bin/bash" "Hidden user Thunderfang removed from the system" "/etc/passwd"
+check_text_exists "/etc/ufw/ufw.conf" "ENABLED=yes" "ufw firewall enabled"
+check_text_exists "/etc/ufw/ufw.conf" "LOGLEVEL=high" "ufw firewall loglevel high"
+check_text_exists "/etc/default/ufw" "IPV6=yes" "Rules support IPV6"
+check_text_exists2 "/etc/ufw/user.rules" "ufw-user-input -p tcp --dport 443 -j ACCEPT" "ufw-user-input -p tcp --dport 80 -j ACCEPT" "ufw allows incoming connections for http and https"
+check_text_exists "/etc/ufw/user.rules" "ufw-user-input -p tcp --dport 3306 -j ACCEPT" "ufw allows incoming connections for MySQL"
+check_text_exists "/etc/login.defs" "PASS_MAX_DAYS[[:space:]]*90" "Password must be changed after 90 days"
+check_text_exists "/etc/login.defs" "LOG_OK_LOGINS[[:space:]]*yes" "logs successful logins"
+check_text_exists "/etc/login.defs" "HOME_MODE[[:space:]]*0750" "new home directory permission set to 0750"
+check_text_not_exists "/etc/rsyslog.conf" "FileGroup nokt" "nokt is not the file group for logs"
+check_text_exists "/etc/rsyslog.conf" 'module(load="imklog" permitnonkernelfacility="on")' "enabled kernel logging support and non-kernel klog messages"
+check_text_exists "/etc/host.conf" "nospoof on" "checks for IP spoofing"
+check_text_exists "/etc/logrotate.conf" "weekly" "Rotates logs regularly"
+check_text_not_exists "/etc/logrotate.conf" "rotate 999" "Does not keep many weeks of backlogs"
+check_text_not_exists "/etc/security/pwquality.conf" "minlen = 8" "Minimum password length is 16"
+check_text_not_exists "/etc/security/pwquality.conf" "dictcheck = 0" "Checks for dictionary words"
+check_text_not_exists "/etc/security/pwquality.conf" "enforcing = 0" "Rejects insecure passwords"
+check_text_exists2 "/etc/apt/apt.conf.d/20auto-upgrades" 'APT::Periodic::Update-Package-Lists "1";' 'APT::Periodic::Unattended-Upgrade "1"' "System set to automatically update"
+check_file_permissions "/etc/sudoers" "440" "Permissions on sudoers file fixed"
+check_text_not_exists "/etc/sudoers" "NOPASSWD" "insecure sudoers rule extinguished"
+check_file_deleted "/etc/sudoers.d/.FINDME" "Hidden sudoers file removed"
+
+
+
+# keep this line at the end, input the path to score report html here
+# accepts two args: path to template html file, and path to actual html file
+_terminate "/etc/scorebot/report-template.html" "/home/sora/Desktop/report.html"
